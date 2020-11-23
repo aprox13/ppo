582bbc9b18191bf5e21551b9329677755546549b
aprox13 <ifkbhit@gmail.com>
2020-11-23 21:22:34 +0300

    save lab5

--- ppo5-mvn/src/main/scala/ru/ifkbhit/ppo5/Main.scala
+++ ppo5-mvn/src/main/scala/ru/ifkbhit/ppo5/Main.scala
@@ -3,16 +3,22 @@ package ru.ifkbhit.ppo5
 import java.io.File
 
 import ru.ifkbhit.ppo5.darwing.{AwtDrawingApi, JavaFxDrawingApi}
-import ru.ifkbhit.ppo5.graph.VerticesGraph
+import ru.ifkbhit.ppo5.graph.{MatrixGraph, VerticesGraph}
 
 object Main {
+
+
   def main(args: Array[String]): Unit = {
 
-    val file = new File(getClass.getClassLoader.getResource("vertices.graph").getPath)
+    val vFile = new File(getClass.getClassLoader.getResource("vertices.graph").getPath)
+    val mFile = new File(getClass.getClassLoader.getResource("matrix.graph").getPath)
 
-    val api = AwtDrawingApi
 
-    val g = VerticesGraph(file, api, 200)
+//    val api = AwtDrawingApi
+    val api = new JavaFxDrawingApi
+
+//    val g = VerticesGraph(vFile, api, 200)
+    val g = MatrixGraph(mFile, api, 200)
     g.draw()
 
 
